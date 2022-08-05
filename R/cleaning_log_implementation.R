b1d5590a44561a62cfeddb5e9877a94b9440180a
Anthony Twesigye <twesigye10@gmail.com>
2022-08-05 10:48:38 +0300

    enhancing filter on the deletion log

--- R/cleaning_log_implementation.R
+++ R/cleaning_log_implementation.R
@@ -9,9 +9,16 @@ source("R/composite_indicators.R")
 # read data
 
 df_cleaning_log <- read_csv("inputs/combined_checks_anif.csv") %>% 
-  mutate(adjust_log = ifelse(is.na(adjust_log), "apply_suggested_change", adjust_log)) %>%
-  filter(adjust_log != "delete_log", !is.na(value), !is.na(uuid)) %>% 
+  filter(!adjust_log %in% c("delete_log")) %>%
   mutate(sheet = NA, index = NA, relevant = NA) %>% 
+  mutate(adjust_log = ifelse(is.na(adjust_log), "apply_suggested_change", adjust_log),
+         value = ifelse(is.na(value) & str_detect(string = issue_id, pattern = "logic_c_"), "blank", value),
+         value = ifelse(type %in% c("remove_survey"), "blank", value),
+         name = ifelse(is.na(name) & type %in% c("remove_survey"), "point_number", name)
+  ) %>%
+  filter(!is.na(value), !is.na(uuid)) %>%
+  mutate(value = ifelse(value %in% c("blank"), NA, value),
+         relevant = NA) %>%
   select(uuid, type, name, value, issue_id, sheet, index, relevant, issue)
 
 data_nms <- names(readxl::read_excel(path = "inputs/ANIF_Rapid_Assessment_Data.xlsx", n_max = 100))
